4b547c0200c4aabbe3078e7e40cc0a3a5ae4cf84
边彦晖 <10646674+bian-yanhui@user.noreply.gitee.com>
2024-11-25 17:54:52 +0800

    更新了流发生器, 可以直接实例化流发生器后调用

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Bg
 
 针对算力网络仿真模拟平台中, 需要在Klonet上实现的广域Collective Communication算法, 结合流发生器给出算法逻辑.
+
+# 流发生器
+
+https://uestc.feishu.cn/docx/OwAJdrn9toxepKxdosdcFq08nwc
--- traffic_generator.py
+++ traffic_generator.py
@@ -1,19 +1,26 @@
-class traffic_generator():
-    """
-    定义简化版的traffic_generator来验证CC Demo
-    """
-    def __init__(self, user_id, proj_id, src_node_id, dst_node_id, data_size, run_time, CONFIG):
-        """
-        这里的src_node_id <-> dst_node_id之间的链路带宽和流量分布应该是从用户给出的CONFIG文件中获取, 但是为了演示Demo这里直接给出
-        """
-        self.src_node_id = src_node_id
-        self.dst_node_id = dst_node_id
-        self.data_size = data_size
-        self.traffic_distribution = "uniform"
-        self.link_bandwidth = 2 #kbps
-    
-    def traffic_generate(self):
-        time = self.data_size / self.link_bandwidth
-        performance_indicators = {"Test":"Test"}
+import requests
+import json
 
-        return time, performance_indicators
+class traffic_generator():
+    def __init__(self, user_id, topo_id, src_node, dst_node, data_size, CONFIG = {}, url = 'http://192.168.1.33:25888/master/traffic_gen/'):
+        
+        self.user_id = user_id
+        self.topo_id = topo_id
+        self.src_node = src_node
+        self.dst_node = dst_node
+        self.data_size = data_size
+        self.CONFIG = CONFIG
+        self.url = url
+
+    def generate(self):
+        data = {
+            "user": self.user_id,
+            "topo": self.topo_id,
+            "src_node": self.src_node,
+            "dst_node": self.dst_node,
+            "data_size": self.data_size,
+            "CONFIG": self.CONFIG
+        }
+        response = requests.post(self.url, json=data)
+        response_json = response.json()
+        return response_json
