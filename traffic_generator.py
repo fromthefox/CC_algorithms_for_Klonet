3af6fdb44c7f6538c05cf21c88dffd6950b65fef
fox <fromthefox@icloud.com>
2024-11-26 10:28:14 +0800

    增加脚本配置Klonet的Demo

--- traffic_generator.py
+++ traffic_generator.py
@@ -1,7 +1,8 @@
 import requests
 
 class traffic_generator():
-    def __init__(self, user_id, topo_id, src_node, dst_node, data_size, CONFIG = {}, url = 'http://192.168.1.33:25888/master/traffic_gen/'):
+    def __init__(self, user_id, topo_id, src_node, dst_node, data_size, CONFIG = {}, url = "http://kb310server.x3322.net:10441/master/traffic_gen/"):
+        # 'http://192.168.1.33:25888/master/traffic_gen/'
         
         self.user_id = user_id
         self.topo_id = topo_id
@@ -10,6 +11,7 @@ class traffic_generator():
         self.data_size = data_size
         self.CONFIG = CONFIG
         self.url = url
+        self.force = 1
 
     def generate(self):
         data = {
@@ -18,14 +20,14 @@ class traffic_generator():
             "src_node": self.src_node,
             "dst_node": self.dst_node,
             "data_size": self.data_size,
+            "force": self.force,
             "CONFIG": self.CONFIG
         }
         response = requests.post(self.url, json=data)
         response_json = response.json()
-        # print(response_json)
-        time = response_json["simple_info"]["time"]
-        return time
-
-
-traffic_generator = traffic_generator("yhbian", "traffic_generator_test", "h1", "h2", "10M")
+        print(response_json)
+        # time = response_json["simple_info"]["time"]
+        # return time
+    
+traffic_generator = traffic_generator("yhbian", "traffic_generator_test", "h1", "h2", "1G")
 traffic_generator.generate()
